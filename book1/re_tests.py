52018b372bbe3733ea261324f3330fd089238162
Denis <den.scherbinin2018@yandex.ru>
2023-04-08 13:05:17 +0500

    start learn HTTP

--- book1/re_tests.py
+++ book1/re_tests.py
@@ -1,1 +1,26 @@
+import re
+
 print('Упражнения, регулярные выражения')
+
+re_command = input('Enter a regular expression: ')
+fhand = open('mbox.txt')
+count = 0
+for line in fhand:
+    line = line.rstrip()
+    if re.search(f'{re_command}', line):
+        count = count + 1
+
+print('Result = ', count)
+
+print('-' * 120)
+
+fhand2 = open('mbox.txt')
+
+l_num = []
+for i in fhand2:
+    i = i.rstrip()
+    x = re.findall('^New Revision\S*: ([0-9.]+)', i)
+    if len(x) > 0:
+        l_num.append(int(x[0]))
+
+print(f'Result = {int(sum(l_num) / len(l_num))}')
